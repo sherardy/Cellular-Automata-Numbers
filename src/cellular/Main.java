affe32c8c087d44a608d12906371032c53b7eee1
Sherardy <sherardy@users.noreply.github.com>
2017-11-29 20:38:15 -0500

    timer stuff

--- src/cellular/Main.java
+++ src/cellular/Main.java
@@ -1,10 +1,24 @@
 public class Main {
 	public static void main(String[] args) {
+		
 		Rand r = new Rand();
-		for (int i = 0; i < 100; i++) {
-			long num = r.getNext() % 10;
-			System.out.println(num);
+		r.seed(2856);
+		double timeAvg, sum=0;
+		int x=0;
+		for (; x<10; x++) {
+			final long start = System.currentTimeMillis();
+			for (int i = 0; i < 100000; i++) {
+				long num = r.getNext() % 10;
+			}
+			final long stop = System.currentTimeMillis();
+			long ms=stop-start;
+			sum+=ms;
+			System.out.println("Execution #"+x+": "+(double)ms/1000.0+" seconds");
 		}
+		timeAvg=sum/x;
+		System.out.println("Average: "+(double)timeAvg/1000.0+" seconds");
+		
+		
 	}
 
 }
